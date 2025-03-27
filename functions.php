fd06cf558f9db355399121cb8c5cd8a65c175e20
fshangala <fshangala@gmail.com>
2025-03-27 12:46:51 +0200

    Replace Tailwind CSS with Bootstrap; update styles and structure for header and hero sections

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function my_block_theme_setup() {
   add_theme_support('wp-block-styles');
   add_theme_support('align-wide');
   add_theme_support('editor-styles');
-  add_editor_style('assets/css/tailwind.css');
+  add_editor_style('assets/css/bootstrap.css');
 
   register_nav_menus([
     'primary_menu' => __('Primary Menu', 'meyo'),
@@ -12,7 +12,7 @@ function my_block_theme_setup() {
 add_action('after_setup_theme', 'my_block_theme_setup');
 
 function meyo_enqueue_styles() {
-  wp_enqueue_style('meyo-tailwind', get_theme_file_uri('/assets/css/tailwind.css'), [], '1.0');
+  wp_enqueue_style('meyo-tailwind', get_theme_file_uri('/assets/css/bootstrap.css'), [], '1.0');
   wp_enqueue_script('meyo-primary-menu', get_template_directory_uri() . '/assets/js/primary-menu.js', [], false, true);
 }
 add_action('wp_enqueue_scripts', 'meyo_enqueue_styles');
